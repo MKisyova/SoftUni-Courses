378878bf24e373fe229527e94bb50b47f19c002a
Milena Kisyova <milena_kissyova@abv.bg>
2022-07-07 20:10:45 +0300

    I added a folder with my exercises from Reflection and Attributes tutorial to C#-OOP folder.

---
+++ C#-OOP/6. Reflection and Attributes/CommandPatternWithReflection/Core/Contracts/CommandInterpreter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core.Contracts
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandInfo = args.Split();
+
+            string commandName = $"{commandInfo[0]}Command";
+
+            Type commandType = Assembly.GetCallingAssembly().GetTypes()
+                .Where(x => x.Name == commandName).FirstOrDefault();
+
+            if (commandType == null)
+            {
+                throw new InvalidOperationException("Invalid command name");
+            }
+
+            ICommand command = Activator.CreateInstance(commandType) as ICommand;
+
+            string result = command.Execute(commandInfo.Skip(1).ToArray());
+
+            return result;
+        }
+    }
+}
---
+++ C#-OOP/6. Reflection and Attributes/CommandPatternWithoutReflection/Core/Contracts/CommandInterpreter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace CommandPattern.Core.Contracts
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] commandInfo = args.Split();
+
+            string commandName = $"{commandInfo[0]}Command";
+            
+            ICommand command = null;
+            string result = null;
+
+            if (commandName == nameof(HelloCommand))
+            {
+                command = new HelloCommand();
+            }
+
+            else if (commandName == "Exit")
+            {
+                command = new ExitCommand();
+            }
+
+            else
+            {
+                throw new InvalidOperationException("Invalid command name");
+            }
+
+            result = command.Execute(commandInfo.Skip(1).ToArray());
+            return result;
+        }
+    }
+}
