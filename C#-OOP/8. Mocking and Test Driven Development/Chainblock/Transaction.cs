182f855938edf365dafe701f33475605c2677f4b
Milena Kisyova <milena_kissyova@abv.bg>
2022-07-27 18:38:59 +0300

    I added a folder with my exercises from Mocking and Test Driven Development tutorial to C#-OOP folder.

---
+++ C#-OOP/8. Mocking and Test Driven Development/Chainblock/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Chainblock.Contracts;
+
+namespace Chainblock
+{
+    public class Transaction : ITransaction
+    {
+        public int Id { get; set; }
+
+        public TransactionStatus Status { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public double Amount { get; set; }
+    }
+}
