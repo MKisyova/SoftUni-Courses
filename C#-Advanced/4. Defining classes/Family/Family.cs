3b1022e41d4dce609d5c24acaf4c684f1d0e3128
Milena Kisyova <milena_kissyova@abv.bg>
2022-09-20 16:42:21 +0300

    I added a folder with my exercises from Defining classes tutorial to C#-Advanced folder.

---
+++ C#-Advanced/4. Defining classes/Family/Family.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+    }
+}
---
+++ C#-Advanced/4. Defining classes/Family/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Vladi";
+            person.Age = 28;
+        }
+    }
+}
