c0d8a02cea860cb63d4a6e7d260f2570062f7148
Milena Kisyova <milena_kissyova@abv.bg>
2022-09-21 22:06:14 +0300

    I added a project in the folder Defining classes (C#-Advanced folder).

---
+++ C#-Advanced/4. Defining classes/OpinionPoll/Family.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; } = new List<Person>();
+
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = People.OrderByDescending(x => x.Age).FirstOrDefault();
+
+            return oldestPerson; 
+        }
+
+        public void AlphabeticallyOrdered()
+        {
+            foreach (var person in People.OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/4. Defining classes/OpinionPoll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] people = Console.ReadLine().Split();
+                string name = people[0];
+                int age = int.Parse(people[1]);
+
+                Person person = new Person(name, age);
+
+                if (person.OlderThanThirty())
+                {
+                    family.AddMember(person);
+                }
+                
+            }
+
+            family.AlphabeticallyOrdered();
+
+        }
+    }
+}
