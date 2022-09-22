c15973b20866bf9f88ed3ace8d1ec469fe6decd0
Milena Kisyova <milena_kissyova@abv.bg>
2022-09-22 16:30:34 +0300

    I added a project in the folder Defining classes (C#-Advanced folder).

---
+++ C#-Advanced/4. Defining classes/DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DayDifference
+{
+    public static class DateModifier
+    {
+        public static int CalculateDateDifference(string firstDate, string secondDate)
+        {
+
+            DateTime dateOne = DateTime.Parse(firstDate);
+            DateTime dateTwo = DateTime.Parse(secondDate);
+
+            int days = Math.Abs((dateOne - dateTwo).Days);
+
+            return days;
+        }
+    }
+}
---
+++ C#-Advanced/4. Defining classes/DateModifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DayDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOne = Console.ReadLine();
+            string dayTwo = Console.ReadLine();
+
+            int days = DateModifier.CalculateDateDifference(dayOne, dayTwo);
+
+            Console.WriteLine(days);
+        }
+    }
+}
