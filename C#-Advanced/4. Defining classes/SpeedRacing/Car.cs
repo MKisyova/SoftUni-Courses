ce62b543b5e2c55a5f5da1fa1e36284e2251561f
Milena Kisyova <milena_kissyova@abv.bg>
2022-09-27 22:40:11 +0300

    I added a project in the folder Defining classes (C#-Advanced folder).

---
+++ C#-Advanced/4. Defining classes/SpeedRacing/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; } = 0;
+
+        public void CarMoving(double amountOfKm)
+        {
+            if (FuelAmount / FuelConsumptionPerKilometer >= amountOfKm)
+            {
+                FuelAmount -= amountOfKm * FuelConsumptionPerKilometer;
+                TravelledDistance += amountOfKm;
+            }
+
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:f2} {TravelledDistance}";
+        }
+    }
+}
---
+++ C#-Advanced/4. Defining classes/SpeedRacing/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeedRacing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < number; i++)
+            {
+                string[] inputInfo = Console.ReadLine().Split(" ");
+
+                string model = inputInfo[0];
+                double fuelAmount = double.Parse(inputInfo[1]);
+                double fuelConsumptionFor1km = double.Parse(inputInfo[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionFor1km);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] driveInfo = command.Split(" ");
+                string carModel = driveInfo[1];
+                double amountOfKm = double.Parse(driveInfo[2]);
+
+                foreach (var car in cars)
+                {
+                    if (car.Model == carModel)
+                    {
+                        car.CarMoving(amountOfKm);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+    }
+}
