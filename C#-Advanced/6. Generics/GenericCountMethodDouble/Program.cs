2e502df5016dbfe7d71d8ed05dfc6c23d6432f37
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-03 23:09:37 +0300

    I added a project in the folder Generics (C#-Advanced folder).

---
+++ C#-Advanced/6. Generics/GenericCountMethodDouble/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<double> elements = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double inputElement = double.Parse(Console.ReadLine());
+                elements.Add(inputElement);
+            }
+
+            double elementToCompare = double.Parse(Console.ReadLine());
+
+            int resultOfCompare = elements.CompareElements(elementToCompare);
+
+            Console.WriteLine(resultOfCompare);
+        }
+    }
+}
