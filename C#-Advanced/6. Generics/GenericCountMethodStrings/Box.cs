b7f3430d0ec4e620fdcaddfd978fd6235f3c562b
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-04 22:08:10 +0300

    I added a project in the folder Generics (C#-Advanced folder).

---
+++ C#-Advanced/6. Generics/GenericCountMethodStrings/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodStrings
+{
+    class Box<T> where T : IComparable
+    {
+        private List<T> items { get; set; }
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            items.Add(element);
+        }
+
+        public int CompareElements(T itemToCompare)
+        {
+            int count = 0;
+
+            foreach (var currentElement in items)
+            {
+                if (currentElement.CompareTo(itemToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
---
+++ C#-Advanced/6. Generics/GenericCountMethodStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericCountMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> elements = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string inputElement = Console.ReadLine();
+                elements.Add(inputElement);
+            }
+
+            string elementToCompare = Console.ReadLine();
+
+            int resultOfCompare = elements.CompareElements(elementToCompare);
+
+            Console.WriteLine(resultOfCompare);
+        }
+    }
+}
