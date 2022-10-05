a6838af2750ef77608a1fb4507119d640f94ad11
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-05 23:20:27 +0300

    I added a project in the folder Generics (C#-Advanced folder).

---
+++ C#-Advanced/6. Generics/GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericSwapMethodIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBoxes = int.Parse(Console.ReadLine());
+
+            Box<int> box = new Box<int>();
+
+            for (int i = 0; i < numberOfBoxes; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+
+            string[] elementsToSwap = Console.ReadLine().Split(" ");
+
+            int firstElementToBeSwapped = int.Parse(elementsToSwap[0]);
+            int secondElementToBeSwapped = int.Parse(elementsToSwap[1]);
+
+            box.Swap(firstElementToBeSwapped, secondElementToBeSwapped);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
