0c40b165ac915ab5fb23a96bc4f2f12933afe1d7
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-06 22:47:07 +0300

    I added a project in the folder Generics (C#-Advanced folder).

---
+++ C#-Advanced/6. Generics/GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    public class Box <T>
+    {
+        private List<T> elements;
+
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            elements.Add(item);
+        }
+
+        public void Swap(int a, int b)
+        {
+            T tempSwappedElement = elements[a];
+            elements[a] = elements[b];
+            elements[b] = tempSwappedElement;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in elements)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#-Advanced/6. Generics/GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBoxes = int.Parse(Console.ReadLine());
+
+            Box<string> box = new Box<string>();
+
+            for (int i = 0; i < numberOfBoxes; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+
+            string[] elementsToSwap = Console.ReadLine().Split(" ");
+
+            int firstElementToBeSwapped = int.Parse(elementsToSwap[0]);
+            int secondElementToBeSwapped = int.Parse(elementsToSwap[1]);
+
+            box.Swap(firstElementToBeSwapped, secondElementToBeSwapped);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
