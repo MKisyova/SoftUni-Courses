e78d73251b36cea0c515270169a8c43f7751d832
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-09 11:13:42 +0300

    I added a project in the folder Generics (C#-Advanced folder).

---
+++ C#-Advanced/6. Generics/Tuple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] personsData = Console.ReadLine().Split();
+
+            string firstName = personsData[0];
+            string lastName = personsData[1];
+            string address = personsData[2];
+            string fullName = $"{firstName} {lastName}";
+
+            Tuple<string, string> person = new Tuple<string, string>(fullName, address);
+
+            string[] beerData = Console.ReadLine().Split();
+
+            string name = beerData[0];
+            int amountOfBeer = int.Parse(beerData[1]);
+
+            Tuple<string, int> amountOfBeerPerPerson = new Tuple<string, int>(name, amountOfBeer);
+
+            string[] numbersData = Console.ReadLine().Split();
+
+            int integerNumber = int.Parse(numbersData[0]);
+            double doubleNumber = double.Parse(numbersData[1]);
+
+            Tuple<int, double> numbers = new Tuple<int, double>(integerNumber, doubleNumber);
+
+            Console.WriteLine(person.ToString());
+            Console.WriteLine(amountOfBeerPerPerson.ToString());
+            Console.WriteLine(numbers.ToString());
+
+        }
+    }
+}
---
+++ C#-Advanced/6. Generics/Tuple/Tuple.cs
@@ -0,0 +1,20 @@
+﻿namespace Tuple
+{
+    class Tuple <T1,T2>
+    {     
+        public T1 FirstItem { get; set; }
+
+        public T2 SecondItem { get; set; }
+
+        public Tuple(T1 firstItem, T2 secondItem)
+        {
+            FirstItem = firstItem;
+            SecondItem = secondItem;
+        }
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem}";
+        }
+
+    }
+}
