ff2d42367983703a354d6ad0212fc359444ae607
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-13 22:41:39 +0300

    I added a project in the folder Iterators and comparators (C#-Advanced folder).

---
+++ C#-Advanced/7. Iterators and comparators/EqualityLogic/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EqualityLogic
+{
+    class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public int CompareTo(Person otherPerson)
+        {
+            int result = name.CompareTo(otherPerson.name);
+
+            if (result == 0)
+            {
+                result = age.CompareTo(otherPerson.age);
+            }
+
+            return result;
+        }
+
+        public override int GetHashCode()
+            => name.GetHashCode() ^ age.GetHashCode();
+        //{
+        //    return HashCode.Combine(name, age);
+        //}
+
+        public override bool Equals(object obj)
+        {
+            var otherPerson = obj as Person;
+
+            if (otherPerson == null)
+            {
+                return false;
+            }
+
+            return age == otherPerson.age && name == otherPerson.name;
+        }
+    }
+}
---
+++ C#-Advanced/7. Iterators and comparators/EqualityLogic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<Person> personsHashSet = new HashSet<Person>();
+            SortedSet<Person> personsSortedSet = new SortedSet<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+
+                personsHashSet.Add(person);
+                personsSortedSet.Add(person);
+            }
+
+            Console.WriteLine(personsSortedSet.Count);
+            Console.WriteLine(personsHashSet.Count);
+        }
+    }
+}
