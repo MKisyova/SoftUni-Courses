14eb04991a50a87f13b0290cb242c0927e0a7253
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-14 10:30:06 +0300

    I added a project in the folder Iterators and comparators (C#-Advanced folder).

---
+++ C#-Advanced/7. Iterators and comparators/Froggy/Lake.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] nums;
+
+        public Lake(int[] numbers)
+        {
+            nums = new int[numbers.Length];
+            nums = numbers;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                yield return nums[i];
+            }
+
+            if (nums.Length % 2 == 0)
+            {
+                for (int i = nums.Length - 1; i >= 1; i -= 2)
+                {
+                    yield return nums[i];
+                }
+            }
+
+            else
+            {
+                for (int i = nums.Length - 2; i >= 1; i -= 2)
+                {
+                    yield return nums[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#-Advanced/7. Iterators and comparators/Froggy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Lake lakeNumbers = new Lake(numbers);
+
+            Console.WriteLine(string.Join(", ", lakeNumbers));
+        }
+    }
+}
