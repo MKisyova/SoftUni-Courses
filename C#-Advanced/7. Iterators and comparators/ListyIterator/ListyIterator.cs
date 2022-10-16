35b0eb8ea5321d5cf9e42d549f5a11ebc7ecf318
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-16 12:23:05 +0300

    I added a project in the folder Iterators and comparators (C#-Advanced folder).

---
+++ C#-Advanced/7. Iterators and comparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    public class ListyIterator <T> : IEnumerable
+    {
+        private List<T> collection;
+        private int currentIndex;
+
+        public ListyIterator(params T[] inputData)
+        {
+            collection = new List<T>(inputData);
+        }
+
+        public bool HasNext() => currentIndex < collection.Count - 1;
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                currentIndex++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            else
+            {
+                Console.WriteLine(collection[currentIndex]);
+            }
+
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine(string.Join(" ", collection));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                yield return collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#-Advanced/7. Iterators and comparators/ListyIterator/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            ListyIterator<string> listy = null;
+
+            while (input != "END")
+            {
+                string[] inputData = input.Split(" ");
+                string action = inputData[0];
+
+                if (action == "Create")
+                {
+                    listy = new ListyIterator<string>(inputData.Skip(1).ToArray());
+                }
+
+                else if (action == "HasNext")
+                {
+                    Console.WriteLine(listy.HasNext());
+                }
+
+                else if (action == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+
+                else if (action == "Print")
+                {
+                    listy.Print();
+                }
+
+                else if (action == "PrintAll")
+                {
+                    listy.PrintAll();
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
