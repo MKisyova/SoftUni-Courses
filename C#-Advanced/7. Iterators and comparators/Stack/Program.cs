dc65110165d961fd6dfc4e57de0a538c4201a586
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-17 19:38:04 +0300

    I added a project in the folder Iterators and comparators (C#-Advanced folder).

---
+++ C#-Advanced/7. Iterators and comparators/Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Stack<string> stackCollection = new Stack<string>();
+
+            while (command != "END")
+            {
+                string[] input = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string action = input[0];
+
+                if (action == "Push")
+                {
+                    string[] elementsToAdd = input.Skip(1).Select(x => x.Split(",").First()).ToArray();
+
+                    stackCollection.Push(elementsToAdd);
+                }
+
+                else if (action == "Pop")
+                {
+                    stackCollection.Pop();
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item in stackCollection)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+}
---
+++ C#-Advanced/7. Iterators and comparators/Stack/Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        public Stack()
+        {
+            collection = new List<T>();
+        }
+
+        public void Push(params T[] input)
+        {
+            foreach (var item in input)
+            {
+                collection.Add(item);
+            }
+        }
+        public void Pop()
+        {
+            if (collection.Count > 0)
+            {
+                collection.RemoveAt(collection.Count - 1);
+            }
+
+            else
+            {
+                Console.WriteLine("No elements");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                yield return collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
