4d59abba89ee25688de6fc497b6be88dc8514f0e
Milena Kisyova <milena_kissyova@abv.bg>
2022-10-29 19:54:36 +0300

    I added Problems Controller and Submissions Controller.

---
+++ C#-Web/Apps/Suls/Controllers/ProblemsController.cs
@@ -0,0 +1,47 @@
+﻿using Suls.Data;
+using Suls.Services;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Suls.Controllers
+{
+    public class ProblemsController : Controller
+    {
+        private readonly IProblemService problemService;
+
+        public ProblemsController(IProblemService problemService)
+        {
+            this.problemService = problemService;
+        }
+
+        public HttpResponse Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Create(string name, ushort points)
+        {
+            if (string.IsNullOrEmpty(name) || name.Length < 5 || name.Length > 20)
+            {
+                return this.Error("Name should be between 5 and 20 characters.");
+            }
+
+            if (points < 50 || points > 300)
+            {
+                return this.Error("Points should be between 50 and 300.");
+            }
+
+            this.problemService.Create(name, points);
+            return this.Redirect("/");
+        }
+
+        public HttpResponse Details(string id)
+        {
+            return this.View();
+        }
+    }
+}
---
+++ C#-Web/Apps/Suls/Controllers/SubmissionsController.cs
@@ -0,0 +1,46 @@
+﻿using Suls.Services;
+using Suls.ViewModels.Submissions;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Suls.Controllers
+{
+    public class SubmissionsController : Controller
+    {
+        private readonly IProblemService problemService;
+        private readonly ISubmissionsService submissionsService;
+
+        public SubmissionsController(IProblemService problemService, ISubmissionsService submissionsService)
+        {
+            this.problemService = problemService;
+            this.submissionsService = submissionsService;
+        }
+
+        public HttpResponse Create(string id)
+        {
+            var viewModel = new CreateViewModel
+            {
+                ProblemId = id,
+                Name = this.problemService.GetNameById(id),
+            };
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public HttpResponse Create(string problemId, string code)
+        {
+            if (string.IsNullOrEmpty(code) || code.Length < 30 || code.Length > 800)
+            {
+                return this.Error("Code should be between 30 and 800 characters.");
+            }
+            var userId = this.GetUserId();
+            this.submissionsService.Create(problemId, userId, code);
+
+            return this.Redirect("/");
+        }
+    }
+}
