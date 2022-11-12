4c850b76aef5f9e7f4c4cad0478fa3cbae685ae3
Milena Kisyova <milena_kissyova@abv.bg>
2022-11-12 16:19:20 +0200

    I added UsersController in Git project.

---
+++ C#-Web/Apps/Git/Controllers/UsersController.cs
@@ -0,0 +1,96 @@
+﻿using Git.Services;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Git.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public HttpResponse Login()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/Repositories/All");
+            }
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Login(string username, string password)
+        {
+            var userId = usersService.GetUserId(username, password);
+            if (userId == null)
+            {
+                return this.Error("Invalid username or password");
+            }
+
+            this.SignIn(userId);
+            return this.Redirect("/Repositories/All");
+        }
+
+
+        public HttpResponse Register()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/Repositories/All");
+            }
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Register(string username, string email, string password, string confirmPassword)
+        {
+            if (string.IsNullOrEmpty(username) || username.Length < 5 || username.Length > 20)
+            {
+                return this.Error("Username should be between 5 and 20 characters.");
+            }
+
+            if (!this.usersService.IsUsernameAvailable(username))
+            {
+                return this.Error("Username already taken.");
+            }
+
+            if (string.IsNullOrEmpty(email) || !new EmailAddressAttribute().IsValid(email))
+            {
+                return this.Error("Invalid email address.");
+            }
+
+            if (!this.usersService.IsEmailAvailable(email))
+            {
+                return this.Error("Email already taken.");
+            }
+
+            if (string.IsNullOrEmpty(password) || password.Length < 6 || password.Length > 20)
+            {
+                return this.Error("Password should be between 6 and 20 characters.");
+            }
+
+            if (password != confirmPassword)
+            {
+                return this.Error("Passwords should match.");
+            }
+
+            this.usersService.CreateUser(username, email, password);
+            return this.Redirect("/Users/Login");
+        }
+
+        public HttpResponse Logout()
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+            this.SignOut();
+            return this.Redirect("/");
+        }
+    }
+}
